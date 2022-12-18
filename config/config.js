858dfab5e4c2e37813807f26c0af66993d5b09e3
trupti-git <60663698+trupti-git@users.noreply.github.com>
2022-12-18 16:34:22 -0500

    Revert "added authroute"

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
-import dotenv from 'dotenv';
-dotenv.config();
 
-export const env = {
-  PORT: process.env.PORT || 3001,
-  DB_URL: process.env.DB_URL,
-  DB_NAME: process.env.DB_NAME,
-  API_LINK: process.env.API_LINK,
-  API_USERS: process.env.API_USERS,
-  SECRET: process.env.SECRET,
+require('dotenv').config();
+
+
+exports.env = {
+    PORT : process.env.PORT || 3001,
+    DB_URL : process.env.DB_URL,
+    DB_NAME : process.env.DB_NAME,
+    API_LINK : process.env.API_LINK,
+    API_USERS: process.env.API_USERS
 };
