5743dce5009180b26c319ae92dd51da6e1b2339f
trupti-git <truptira85@gmail.com>
2022-12-25 23:18:26 -0500

    added lint script

--- app.js
+++ app.js
@@ -33,54 +33,51 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocuments));
 
 // To check health of the application, is it up / down. Usually load balancer use this
 app.get('/health', (req, res) => {
-    res.send('Healthy');
-})
+  res.send('Healthy');
+});
 
 //app.use('/',router);
-app.use('/',accountRouter.router);
+app.use('/', accountRouter.router);
 // console.log(accountRoute.size);
-
 
 ///////
 
 //app.use(middleware1);
 app.use(middleware1);
 
-app.get('/users',auth, (req,res) => {
+app.get('/users', auth, (req, res) => {
   console.log('This is standard api');
   res.send('users page');
-  
 });
 
 //app.use(middleware3);
 
-function auth(req,res,next) {
-   
+function auth(req, res, next) {
   if (req.query.admin === 'true') {
     console.log('auth user');
     next();
   }
-    console.log('No auth');
-    next();
-  
+  console.log('No auth');
+  next();
 }
 
-function middleware1(req,res,next) {
-    console.log('This is middleware #1');
-    next();
-  }
-  
-function middleware2(req,res,next) {
+function middleware1(req, res, next) {
+  console.log('This is middleware #1');
+  next();
+}
+
+function middleware2(req, res, next) {
   console.log('This is middleware #2');
   //next();
 }
 
-function middleware3(req,res,next) {
-    console.log('This is middleware #3');
-    next();
-  }
+function middleware3(req, res, next) {
+  console.log('This is middleware #3');
+  next();
+}
 
 ///////
 
-
-app.listen(env.PORT,()=> console.log(`Server connection successful on port ${env.PORT}`));
+app.listen(env.PORT, () =>
+  console.log(`Server connection successful on port ${env.PORT}`)
+);
--- config/config.js
+++ config/config.js
@@ -1,11 +1,9 @@
-
 require('dotenv').config();
 
-
 exports.env = {
-    PORT : process.env.PORT || 3001,
-    DB_URL : process.env.DB_URL,
-    DB_NAME : process.env.DB_NAME,
-    API_LINK : process.env.API_LINK,
-    API_USERS: process.env.API_USERS
+  PORT: process.env.PORT || 3001,
+  DB_URL: process.env.DB_URL,
+  DB_NAME: process.env.DB_NAME,
+  API_LINK: process.env.API_LINK,
+  API_USERS: process.env.API_USERS,
 };
